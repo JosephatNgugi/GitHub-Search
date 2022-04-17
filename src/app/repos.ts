0f5414df571d411664d307a338420da7a63b2fde
JosephatNgugi <Josephat.muriu@student.moringaschool.com>
2022-04-17 23:10:31 +0300

    add repos and user classes blueprint

---
+++ src/app/repos.ts
@@ -0,0 +1,9 @@
+export class Repos {
+    constructor(
+        public repoName: string,
+        public description: string,
+        public repoLink: string,
+        public liveLink: string,
+        public repoDate: Date
+    ){}
+}
--- src/app/users.ts
+++ src/app/users.ts
@@ -1,2 +1,11 @@
 export class Users {
+  constructor(
+    public userName: string,
+    public type: string,
+    public avatarUrl: string,
+    public bio: string,
+    public following: number,
+    public followers: number,
+    public creationDate: Date
+  ) {}
 }
